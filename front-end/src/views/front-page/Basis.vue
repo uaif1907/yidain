053d18ad120a27e397b693d947bbea9434422606
z18406565980 <130110945@qq.com>
2019-11-05 16:43:01 +0800

    初始化

---
+++ front-end/src/views/front-page/Basis.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    头和尾的页面
+    <router-view></router-view>
+  </div>
+
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'home',
+  components: {
+
+  }
+}
+</script>
---
+++ front-end/src/views/front-page/Person.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    个人中心
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'home',
+  components: {
+
+  }
+}
+</script>
